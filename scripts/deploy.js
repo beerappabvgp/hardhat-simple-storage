c63f685e34f6f248c731dcf73d3d728a36eddd75
beerappabvgp <beerappabharathb@gmail.com>
2024-04-10 09:04:32 +0530

    Deployed simple storage to sepolia testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,11 @@
-// imports 
+const { ethers } = require("hardhat");
 
 const main = async () => {
-
+    const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    console.log("Deploying contract");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    await simpleStorage.waitForDeployment();
+    console.log("deployed");
 }
 
 main().then(() => process.exit(0))
